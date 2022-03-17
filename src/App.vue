06b2356b8584d3da28cb650a84521018f3e92a26
oncefirefly <oncefirefly@gmail.com>
2022-03-17 17:30:06 +0300

    filter/pagination

--- src/App.vue
+++ src/App.vue
@@ -85,10 +85,28 @@
         </button>
       </section>
       <template v-if="tickers.length">
+        <div>
+          <hr class="w-full border-t border-gray-600 my-4" />
+          <button
+            v-if="page > 1"
+            @click="page = page - 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -184,17 +202,32 @@ export default {
       alreadyAdded: false,
       renderingPage: true,
       coinsList: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
     };
   },
 
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("cryptonomicon-list");
 
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
-      this.tickers.forEach((ticker) => {
-        this.subscribeToUpdates(ticker.name);
-      });
+      // this.tickers.forEach((ticker) => {
+      //   this.subscribeToUpdates(ticker.name);
+      // });
     }
   },
 
@@ -237,9 +270,23 @@ export default {
         this.ticker = "";
       }
 
+      this.filter = "";
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
 
-      this.subscribeToUpdates(newTicker.name);
+      // this.subscribeToUpdates(newTicker.name);
+    },
+
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredT = this.tickers.filter((ticker) =>
+        ticker.name.toLowerCase().includes(this.filter.toLowerCase())
+      );
+
+      this.hasNextPage = filteredT.length > end;
+
+      return filteredT.slice(start, end);
     },
 
     select(ticker) {
@@ -286,6 +333,25 @@ export default {
 
   mounted: function () {
     this.renderingPage = false;
+  },
+
+  watch: {
+    filter() {
+      this.page = 1;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
   },
 };
 </script>
