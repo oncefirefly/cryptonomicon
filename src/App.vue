5519f5b08b507932492ec176a4dc21d6a1819675
oncefirefly <oncefirefly@gmail.com>
2022-05-03 15:46:41 +0300

    stashed changes applied

--- src/App.vue
+++ src/App.vue
@@ -32,79 +32,12 @@
         :alreadyAdded="alreadyAdded"
         :tickers="tickers"
       />
-<<<<<<< Updated upstream
-      <template v-if="tickers.length">
-        <div>
-          <hr class="w-full border-t border-gray-600 my-4" />
-          <button
-            v-if="page > 1"
-            @click="page = page - 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Назад
-          </button>
-          <button
-            v-if="hasNextPage"
-            @click="page = page + 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Вперед
-          </button>
-          <div>Фильтр: <input v-model="filter" /></div>
-        </div>
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
-            v-for="t in paginatedTickers"
-            :key="t.name"
-            @click="select(t)"
-            :class="{
-              'border-4': selectedTicker === t,
-            }"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-          >
-            <div
-              class="px-4 py-5 sm:p-6 text-center"
-              :class="{ 'bg-red-300 opacity-50': t.price === '-' }"
-            >
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
-              </dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ formatPrice(t.price) }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <button
-              @click.stop="deleteTicker(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
-            </button>
-          </div>
-        </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
-      </template>
-=======
       <my-tickers
         @select-ticker="select"
         @delete-ticker="deleteTicker"
         :tickers="tickers"
         :selectedTicker="selectedTicker"
       />
->>>>>>> Stashed changes
       <ticker-graph
         @close-graph="closeGraph"
         @update-graph="updateGraph"
